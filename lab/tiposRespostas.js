6969340e63221561b80b7d5c8edc516af244bb75
Douglas <douglasbelinato@cvccorp.com.br>
2020-06-11 15:15:57 -0300

    Formas de responder com Reply

---
+++ lab/tiposRespostas.js
@@ -0,0 +1,18 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.start(async ctx => {
+    await ctx.reply(`Seja bem vindo, ${ctx.update.message.from.first_name}! 😁`)
+    await ctx.replyWithHTML(`Destacando mensagem <b>HTML</b> <i>de</i> <code>várias</code> <pre>formas possíveis</pre>
+                            <a href="http://www.google.com.br">Google</a>`)
+    await ctx.replyWithMarkdown('Destacando mensagem *Markdown* _de_ `várias formas` ```possíveis```' +
+                                '[Google](http://www.google.com)')
+    await ctx.replyWithPhoto({source: `${__dirname}/images/dog_cat.jpeg`})
+    await ctx.replyWithPhoto('https://i.picsum.photos/id/685/200/200.jpg', {caption: 'Linda foto!'})
+    await ctx.replyWithPhoto({url: 'https://i.picsum.photos/id/985/200/200.jpg'})
+    await ctx.replyWithLocation(29.9773008, 31.1303068)
+    // await ctx.replyWithVideo('http://files.cod3er.com.br/curso-bot/cod3r-end.m4')
+})
+
+bot.startPolling()
