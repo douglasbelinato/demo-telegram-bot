a679a1a19ca67085ccbaacf9b7b73ee5c99a4efb
Douglas <douglasbelinato@cvccorp.com.br>
2020-06-15 18:38:26 -0300

    Enviando mensagens assíncronas

---
+++ avancados/mensagensAssincronas.js
@@ -0,0 +1,21 @@
+const env = require('../.env')
+const Telegram = require('telegraf/telegram')
+const Markup = require('telegraf/markup')
+const axios = require('axios')
+
+const chatId = env.authorized_client_ids[0]
+console.log(chatId)
+
+const enviarMensagem = msg => {    
+    axios.get(`${env.apiUrl}/sendMessage?chat_id=${chatId}&text=${encodeURI(msg)}`)
+        .catch(e => console.log(e))
+}
+
+enviarMensagem('Enviando msg assíncrona')
+
+const teclado = Markup.keyboard([
+    ['OK', 'Cancelar']
+]).resize().oneTime().extra()
+
+const telegram = new Telegram(env.token)
+telegram.sendMessage(chatId, 'Essa é uma msg com teclado', teclado)
